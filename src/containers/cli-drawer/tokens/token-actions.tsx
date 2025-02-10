de2a9a9c598bd588e65b06c763fb4d0d8c4355ed
Ben Elferink <ben.elferink@icloud.com>
2025-02-10 16:14:56 +0200

    fix: import error

--- src/containers/cli-drawer/tokens/token-actions.tsx
+++ src/containers/cli-drawer/tokens/token-actions.tsx
@@ -1,5 +1,6 @@
 import React, { type FC, useRef, useState } from 'react'
-import Theme, { styled } from '@odigos/ui-theme'
+import Theme from '@odigos/ui-theme'
+import styled from 'styled-components'
 import { CheckIcon, CopyIcon, CrossIcon, EditIcon } from '@odigos/ui-icons'
 import { getStatusIcon, NOTIFICATION_TYPE, useCopy, useOnClickOutside } from '@odigos/ui-utils'
 import { Button, Divider, FlexColumn, FlexRow, IconButton, Input, Text, Tooltip } from '@odigos/ui-components'
